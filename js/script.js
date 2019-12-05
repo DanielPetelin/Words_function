4624f3231d6a2a8b982ef908f38b4e951e8b754a
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-05 20:36:03 +0300

    Слова

--- js/script.js
+++ js/script.js
@@ -1,15 +1,7 @@
-//function countLines([]) {
-    //for (var i = 1; i < Array.length; i++) {
-        //console.log(i);
-    //}
-//}
-
-//countLines(['Петя', 'Джон', 'Вася', 'катя', 'Боб']);
-
 function countPersons(persons) {
     for (var i = 1; i < persons.length + 1; i++) {
         console.log(i);
     }
 };
 
-countPersons(['Петя', 'Валя', 'Вася', 'Джек', 'Пупкин']);
+countPersons(['Андрей', 'Данил', 'Фаин', 'Котик']);
