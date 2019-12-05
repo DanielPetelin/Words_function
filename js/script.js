7bb69efb9fe8884ff3cffcbeeaa264e811425842
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-05 20:48:50 +0300

    /

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-var a = 12;
-var d = 15;
-var k = 18;
-var m = 3;
-var f = a + d;
-var g = k + m;
-var l = f + g;
-
-console.log(g + f);
-console.log(l);
+//var a = 12;
+//var d = 15;
+//var k = 18;
+//var m = 3;
+//var f = a + d;
+//var g = k + m;
+//var l = f + g;
+//не надо
+//console.log(g + f);
+//console.log(l);
 
 function countPersons(persons) {
     for (var i = 1; i < persons.length + 1; i++) {
